70db9f767d2cdb909105d3b02ac8c5b3b881f840
Kennydasilva <kennydasilva542@gmail.com>
2026-09-24 09:37:58 +0200

    Criar slide de projectos

---
+++ src/slides/Projects.tsx
@@ -0,0 +1,16 @@
+import Eyebrow from '../components/Eyebrow'
+
+export default function Projects() {
+  return (
+    <section className="slide projects" data-label="Projectos">
+      <Eyebrow>PROJECTOS</Eyebrow>
+      <div className="projects__mark" aria-hidden="true">
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+      <h2>Brevemente</h2>
+    </section>
+  )
+}
